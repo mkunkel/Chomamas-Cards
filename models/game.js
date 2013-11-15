4e50d8165a01d7b8c639b53c004a7c98d310e4ff
Lori McCurry <lori.mccurry@gmail.com>
2013-11-15 11:09:55 -0600

    model setup

--- models/game.js
+++ models/game.js
@@ -1,19 +1,21 @@
 var mongoose = require('mongoose');
 var __ = require('lodash');
 
-var Game = mongoose.Schema({
-  name:      String,
-  players:   [{type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
-  createdAt: {type: Date, default: Date.now}
-});
-
-
 function randomize() {
   return __.sample(__.range(10));
 }
 
-Game.pre('save', function(next){
+var Game = mongoose.Schema({
+  name:      String,
+  players:   [{type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
+  submissions: [],
+  whiteCards: [Number],
+  blackCards: [Number]
+});
 
+
+Game.pre('save', function(next){
+  next();
 });
 
 mongoose.model('Game', Game);
